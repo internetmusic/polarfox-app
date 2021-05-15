d363e59f7ccec97b725d276e4ba45da0cfa78cbf
NHebrard <nicolas.hebrard@outlook.com>
2021-05-15 13:11:42 +0200

    Update PFX and StakingRewards contracts import

--- src/constants/abis/staking-rewards.ts
+++ src/constants/abis/staking-rewards.ts
@@ -1,6 +1,6 @@
 import { Interface } from '@ethersproject/abi'
 // TODO: This should be in the core, not in the governance
-import { abi as STAKING_REWARDS_ABI } from '@polarfox/governance/artifacts/contracts/StakingRewards.sol/StakingRewards.json'
+import { abi as STAKING_REWARDS_ABI } from '@polarfox/core/build/StakingRewards.json'
 
 const STAKING_REWARDS_INTERFACE = new Interface(STAKING_REWARDS_ABI)
 
