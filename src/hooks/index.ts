16a9e0521ae4a9f9ab3312620c3620b6c71968ba
internetmusic <thor@internetmusic.ltd.uk>
2021-08-16 15:04:43 +0200

    UpdateFirst

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,11 +1,11 @@
-import { Web3Provider } from '@ethersproject/providers'
-import { ChainId } from '@polarfox/sdk'
-import { useWeb3React as useWeb3ReactCore } from '@web3-react/core'
-import { Web3ReactContextInterface } from '@web3-react/core/dist/types'
-import { useEffect, useState } from 'react'
-import { isMobile } from 'react-device-detect'
-import { injected } from '../connectors'
-import { NetworkContextName } from '../constants'
+import {Web3Provider} from '@ethersproject/providers'
+import {ChainId} from '@polarfox/sdk'
+import {useWeb3React as useWeb3ReactCore} from '@web3-react/core'
+import {Web3ReactContextInterface} from '@web3-react/core/dist/types'
+import {useEffect, useState} from 'react'
+import {isMobile} from 'react-device-detect'
+import {injected} from '../connectors'
+import {NetworkContextName} from '../constants'
 
 export function useActiveWeb3React(): Web3ReactContextInterface<Web3Provider> & { chainId?: ChainId } {
   const context = useWeb3ReactCore<Web3Provider>()
@@ -47,7 +47,7 @@ export function useEagerConnect() {
 
 /**
  * Use for network and injected - logs user in
- * and out after checking what network theyre on
+ * and out after checking what network they are on
  */
 export function useInactiveListener(suppress = false) {
   const { active, error, activate } = useWeb3ReactCore() // specifically using useWeb3React because of what this hook does
